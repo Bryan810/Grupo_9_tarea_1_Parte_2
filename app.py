3c0d18fc46dbe3dfe4067f17f08d3d9e109a4cc2
Bryan8101 <bryansaltos810@gmail.com>
2025-08-18 22:08:47 -0500

    Added screenshots for project

--- app.py
+++ app.py
@@ -73,5 +73,6 @@ def pokemon_aleatorio():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
